056cc55c03ce6a90d1cba82848bc3ff0f0ceb89c
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2020-10-16 17:32:10 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/mateus/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,36 @@
+package com.mateus.cursomc.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa fisica"), PESSOAJURIDICA(2, "Pessoa Juridica");
+
+	private int cod;
+	private String descricao;
+
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static TipoCliente toEnum(Integer id) {
+
+		if (id == null) {
+			return null;
+		}
+
+		for (TipoCliente x : TipoCliente.values()) {
+			if (id.equals(x.getCod())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Id inválido " + id);
+	}
+}
