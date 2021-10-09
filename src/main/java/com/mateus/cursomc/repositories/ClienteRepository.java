2d2e1785ced6564143f3e818cc07a470fc6a92a0
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-10-08 23:33:57 -0300

    Validação customizada: email não repetido na inserção de cliente

--- src/main/java/com/mateus/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/mateus/cursomc/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.mateus.cursomc.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.mateus.cursomc.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/mateus/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/mateus/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mateus.cursomc.domain.Cliente;
 import com.mateus.cursomc.domain.enums.TipoCliente;
 import com.mateus.cursomc.dto.ClienteNewDTO;
+import com.mateus.cursomc.repositories.ClienteRepository;
 import com.mateus.cursomc.resources.exceptions.FieldMessage;
 import com.mateus.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("CpfOuCnpj", "CNPJ invalido"));
 		}
 
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
