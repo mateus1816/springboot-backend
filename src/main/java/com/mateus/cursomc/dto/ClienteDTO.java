8c6643a559e3a4e236703b60b2517e60305ce667
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-10-09 23:50:40 -0300

    Validação customizada: email não repetido na atualização de cliente

--- src/main/java/com/mateus/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/mateus/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.mateus.cursomc.domain.Cliente;
+import com.mateus.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
