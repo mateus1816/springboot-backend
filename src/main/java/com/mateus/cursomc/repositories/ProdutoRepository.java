617023be88850f693267bc97138c24a8a7c80cf9
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-10-14 23:54:16 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/mateus/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/mateus/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,20 @@
 package com.mateus.cursomc.repositories;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.mateus.cursomc.domain.Categoria;
 import com.mateus.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
---
+++ src/main/java/com/mateus/cursomc/services/ProdutoService.java
@@ -0,0 +1,40 @@
+package com.mateus.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.mateus.cursomc.domain.Categoria;
+import com.mateus.cursomc.domain.Produto;
+import com.mateus.cursomc.repositories.CategoriaRepository;
+import com.mateus.cursomc.repositories.ProdutoRepository;
+import com.mateus.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository repo;
+
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+			String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+
+}
