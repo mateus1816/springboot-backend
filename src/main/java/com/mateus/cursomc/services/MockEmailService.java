545968c2b38fc8c9d0ba86373b175da1b2d87276
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-11-02 21:59:51 -0300

    Email HTML

--- src/main/java/com/mateus/cursomc/services/MockEmailService.java
+++ src/main/java/com/mateus/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.mateus.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -7,10 +9,17 @@ import org.springframework.mail.SimpleMailMessage;
 public class MockEmailService extends AbstractEmailService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
-	
+
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 	}
