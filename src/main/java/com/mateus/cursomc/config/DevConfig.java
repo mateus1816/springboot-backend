78558ca61d74d86a8f1801638e322537452b9eb8
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-11-02 21:13:24 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/mateus/cursomc/config/DevConfig.java
+++ src/main/java/com/mateus/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.mateus.cursomc.services.DBService;
+import com.mateus.cursomc.services.EmailService;
+import com.mateus.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,6 +30,11 @@ public class DevConfig {
 		}
 		dbService.instantiateDataBase();
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
