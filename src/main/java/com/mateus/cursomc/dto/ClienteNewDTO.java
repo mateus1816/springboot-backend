b9d4e25ee9210a881fc537a9d1c28c7b5dd4685c
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-11-09 12:22:31 -0300

    Adicionando senha a Cliente

--- src/main/java/com/mateus/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/mateus/cursomc/dto/ClienteNewDTO.java
@@ -23,14 +23,20 @@ public class ClienteNewDTO implements Serializable {
 
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cpfOuCnpj;
+
 	private Integer tipo;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String senha;
 
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
 
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
+
 	private String complemento;
+
 	private String bairro;
 
 	@NotEmpty(message = "Preenchimento obrigatório")
@@ -38,7 +44,9 @@ public class ClienteNewDTO implements Serializable {
 
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String telefone1;
+
 	private String telefone2;
+
 	private String telefone3;
 
 	private Integer cidadeId;
@@ -76,6 +84,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setTipo(Integer tipo) {
 		this.tipo = tipo;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public String getNumero() {
