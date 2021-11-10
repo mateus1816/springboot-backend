d5b8a952c38e03b6c6dfd96272339b5abe472031
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-11-10 14:41:45 -0300

    Implementando autorização

--- src/main/java/com/mateus/cursomc/config/SecurityConfig.java
+++ src/main/java/com/mateus/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mateus.cursomc.security.JWTAuthenticationFilter;
+import com.mateus.cursomc.security.JWTAuthorizationFilter;
 import com.mateus.cursomc.security.JWTUtil;
 
 @Configuration
@@ -51,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
