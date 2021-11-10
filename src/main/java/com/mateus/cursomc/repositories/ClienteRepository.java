ee3063daacf6a741c43b4543f9c1c169dc017289
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-11-10 17:11:09 -0300

    Restrição de conteudo: cliente só recupera seus pedidos

--- src/main/java/com/mateus/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/mateus/cursomc/repositories/ClienteRepository.java
@@ -1,15 +1,14 @@
 package com.mateus.cursomc.repositories;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mateus.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
-	@Transactional
+	@Transactional(readOnly = true)
 	Cliente findByEmail(String email);
 }
--- src/main/java/com/mateus/cursomc/services/PedidoService.java
+++ src/main/java/com/mateus/cursomc/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.mateus.cursomc.domain.Cliente;
 import com.mateus.cursomc.domain.ItemPedido;
 import com.mateus.cursomc.domain.PagamentoComBoleto;
 import com.mateus.cursomc.domain.Pedido;
@@ -15,6 +19,8 @@ import com.mateus.cursomc.domain.enums.EstadoPagamento;
 import com.mateus.cursomc.repositories.ItemPedidoRepository;
 import com.mateus.cursomc.repositories.PagamentoRepository;
 import com.mateus.cursomc.repositories.PedidoRepository;
+import com.mateus.cursomc.security.UserSS;
+import com.mateus.cursomc.services.exceptions.AuthorizationException;
 import com.mateus.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -69,5 +75,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);	
 	}
 }
