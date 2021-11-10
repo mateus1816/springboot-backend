283d1e2c4d2ba72c2e10f4fc3dba62cd4df15092
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-11-10 18:59:02 -0300

    Esqueci a senha

--- src/main/java/com/mateus/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/mateus/cursomc/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.mateus.cursomc.domain.Cliente;
 import com.mateus.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -42,6 +43,22 @@ public abstract class AbstractEmailService implements EmailService {
 		return sm;
 	}
 
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+	
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
+	}
+
 	protected String htmlFromTemplatePedido(Pedido obj) {
 		Context context = new Context();
 		context.setVariable("pedido", obj);
@@ -68,4 +85,5 @@ public abstract class AbstractEmailService implements EmailService {
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
 	}
+	
 }
