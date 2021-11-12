49322e97cdcd7edf936e5c0b9ee7b49eade7ac19
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-11-12 12:15:09 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/mateus/cursomc/domain/Cliente.java
+++ src/main/java/com/mateus/cursomc/domain/Cliente.java
@@ -50,6 +50,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "PERFIS")
 	private Set<Integer> perfis = new HashSet<>();
 
+	private String imageUrl;
+	
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
@@ -172,6 +174,14 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 
 }
